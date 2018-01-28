f8f04687a2c992ab9ffb029393f07afdce20330d
Priyankar Kumar <priyankar.kumar98@gmail.com>
2018-01-28 16:41:21 +0530

    First and Last commit

---
+++ nodeMong/index.js
@@ -0,0 +1,76 @@
+const mongoose=require('mongoose');
+mongoose.Promise = require('bluebird');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url, {
+    useMongoClient: true
+});
+
+connect.then((db) => {
+
+    console.log('Connected correctly to server');
+
+  /*  var newDish = Dishes({
+        name: 'Uthappizza',
+        description: 'test'
+    });
+
+    newDish.save()
+        .then((dish) => {
+            console.log(dish);
+
+            return Dishes.find({}).exec();
+        })
+        .then((dishes) => {
+            console.log(dishes);
+
+            return db.collection('dishes').drop();
+        })
+        .then(() => {
+            return db.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+   */
+
+    Dishes.create({
+        name: 'Uthappam',
+        description: 'test'
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.concat([{
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        }]);
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return db.collection('dishes').drop();
+    })
+    .then(() => {
+        return db.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+});
